cbd3da3066e0093449afcaecb858ad11cbac80ea
Sumit Tripathi <sumit.tpt07@gmail.com>
2024-11-23 12:16:10 +0530

    empty

--- app/generate/page.jsx
+++ app/generate/page.jsx
@@ -7,7 +7,7 @@ const page = () => {
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <div className="flex flex-col ">
         <p className="relative z-20 py-8 text-4xl font-bold text-transparent sm:text-7xl bg-clip-text bg-gradient-to-b from-neutral-200 to-neutral-500">
-          Short the URL!
+          Short the URL!!
         </p>
         <Form />
       </div>
